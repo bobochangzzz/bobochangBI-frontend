8c42160ed80324567fce435f63e9222650a398f4
bobochang <m13726750698_2@163.com>
2023-05-25 11:49:58 +0800

    feat: 添加开放平台界面

--- config/routes.ts
+++ config/routes.ts
@@ -10,6 +10,7 @@ export default [
   { icon: 'smile', name: '欢迎页', path: '/welcome', component: './Welcome' },
   { icon: 'barChart', name: '智能分析', path: '/chart', component: './Chart' },
   { icon: 'user', path: '/user/info', component: './User/Info' },
+  { icon: 'deploymentUnit', path: '/user/invoke', component: './User/Invoke' },
   { path: '/', redirect: '/welcome' },
   { name: '错误页', path: '*', layout: false, component: './404' },
 ];
